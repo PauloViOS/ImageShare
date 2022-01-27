58dd064b9e31c9c7e80a59942d780647cae528f8
Paulo Santos <paulo.santos@buser.com.br>
2022-01-27 00:12:30 -0300

    Added button and logo

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/>
+      <Text style={styles.instructions}>
+        Para compartilhar uma foto do seu celular com um amigo, aperte o botão abaixo
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Olá, mundo')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Escolha uma foto</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +25,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305, 
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color:'#fff',
+  },
+
 });
